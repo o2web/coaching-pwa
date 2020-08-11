cd2a3a4592086ca7541d85e4ac55928648bc3f32
bedardo <obedard@o2web.ca>
2020-08-11 13:04:32 -0400

    Update exercise

--- js/app.js
+++ js/app.js
@@ -4,4 +4,11 @@ const onload = () => {
 	const filter = new Filter;
 };
 
+const getFilterValue = () => {
+	const filtersValue = document.getElementById('filters').value;
+	const searchValue = document.getElementById('search').value;
+	alert(`Filter value = ${filtersValue} \nField value = ${searchValue}`);
+};
+
 window.onload = onload;
+document.getElementById('filters-form').onsubmit = getFilterValue;
